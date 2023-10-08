6063e4e0e7537f84197ffa8e342be9daa3bfb735
Kariny <araujookariny@gmail.com>
2023-10-07 23:03:19 -0300

    projeto react

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import CartButton from '../CartButton/CartButton';
+import SearchBar from '../SearchBar/SearchBar';
+
+import './Header.css';
+
+function Header() {
+  return (
+    <header className="header">
+      <div className="container">
+        <SearchBar />
+        <CartButton />
+      </div>
+    </header>
+    
+  );
+  
+}
+
+export default Header;
